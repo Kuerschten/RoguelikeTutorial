a385ee2ff73f097bf5efba2583af0ff91e8715ea
kuerschten <dk-siggi@web.de>
2021-02-23 17:27:35 +0100

    Refactoring dungeon generation

---
+++ dungeon/constants.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from typing import Dict, List, Tuple, TYPE_CHECKING
+
+import entity_factories
+
+
+if TYPE_CHECKING:
+    from entity import Entity
+
+
+max_items_by_floor = [
+    (1, 1),
+    (4, 2)
+]
+
+max_monsters_by_floor = [
+    (1, 2),
+    (4, 3),
+    (6, 5)
+]
+
+item_chances: Dict[int, List[Tuple[Entity, int]]] = {
+    0: [(entity_factories.health_potion, 35)],
+    2: [(entity_factories.confusion_scroll, 10)],
+    4: [(entity_factories.lightning_scroll, 25), (entity_factories.sword, 5)],
+    6: [(entity_factories.fireball_scroll, 25), (entity_factories.chain_mail, 15)],
+}
+
+enemy_chances: Dict[int, List[Tuple[Entity, int]]] = {
+    0: [(entity_factories.orc, 80)],
+    3: [(entity_factories.troll, 15)],
+    5: [(entity_factories.troll, 30)],
+    7: [(entity_factories.troll, 60)],
+}
