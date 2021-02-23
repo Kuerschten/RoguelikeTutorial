0d3d94612133bb7ba21dfb75834cde5fb44ab0bd
kuerschten <dk-siggi@web.de>
2021-02-23 16:52:22 +0100

    Part 13 - Gearing up

--- entity_factories.py
+++ entity_factories.py
@@ -1,5 +1,6 @@
 from components.ai import HostileEnemy
-from components import consumable
+from components import consumable, equippable
+from components.equipment import Equipment
 from components.fighter import Fighter
 from components.inventory import Inventory
 from components.level import Level
@@ -10,7 +11,8 @@ player = Actor(
     color=(255, 255, 255),
     name="Player",
     ai_cls=HostileEnemy,
-    fighter=Fighter(hp=30, defense=2, power=5),
+    equipment=Equipment(),
+    fighter=Fighter(hp=30, base_defense=1, base_power=2),
     inventory=Inventory(capacity=26),
     level=Level(level_up_base=200)
 )
@@ -21,16 +23,19 @@ orc = Actor(
     color=(63, 127, 63),
     name="Orc",
     ai_cls=HostileEnemy,
-    fighter=Fighter(hp=10, defense=0, power=3),
+    equipment=Equipment(),
+    fighter=Fighter(hp=10, base_defense=0, base_power=3),
     inventory=Inventory(capacity=0),
     level=Level(xp_given=35)
 )
+
 troll = Actor(
     char="T",
     color=(0, 127, 0),
     name="Troll",
     ai_cls=HostileEnemy,
-    fighter=Fighter(hp=16, defense=1, power=4),
+    equipment=Equipment(),
+    fighter=Fighter(hp=16, base_defense=1, base_power=4),
     inventory=Inventory(capacity=0),
     level=Level(xp_given=100)
 )
@@ -42,19 +47,55 @@ confusion_scroll = Item(
     char=scroll_char,
     color=(207, 63, 255),
     name="Confusion Scroll",
-    consumable=consumable.ConfusionConsumable(number_of_turns=10),
+    consumable=consumable.ConfusionConsumable(number_of_turns=10)
 )
+
 lightning_scroll = Item(
     char=scroll_char,
     color=(255, 255, 0),
     name="Lightning Scroll",
-    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
+    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5)
 )
+
 fireball_scroll = Item(
     char=scroll_char,
     color=(255, 0, 0),
     name="Fireball Scroll",
-    consumable=consumable.FireballDamageConsumable(damage=12, radius=3),
+    consumable=consumable.FireballDamageConsumable(damage=12, radius=3)
+)
+
+# weapons
+weapon_char = ")"
+
+dagger = Item(
+    char=weapon_char,
+    color=(0, 191, 255),
+    name="Dagger",
+    equippable=equippable.Dagger()
+)
+
+sword = Item(
+    char=weapon_char,
+    color=(0, 191, 255),
+    name="Sword",
+    equippable=equippable.Sword()
+)
+
+# armors
+armor_char = "["
+
+leather_armor = Item(
+    char=armor_char,
+    color=(139, 69, 19),
+    name="Leather Armor",
+    equippable=equippable.LeatherArmor()
+)
+
+chain_mail = Item(
+    char=armor_char,
+    color=(139, 69, 19),
+    name="Chain Mail",
+    equippable=equippable.ChainMail()
 )
 
 # potions
@@ -64,5 +105,5 @@ health_potion = Item(
     char=potion_char,
     color=(127, 0, 255),
     name="Health Potion",
-    consumable=consumable.HealingConsumable(amount=4),
+    consumable=consumable.HealingConsumable(amount=4)
 )
